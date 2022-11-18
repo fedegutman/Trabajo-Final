ac28b3e981a8dac236ec5db5457c43ad87615210
fedegutman <fedegutman@udesa.edu.ar>
2022-11-18 09:29:01 -0300

    prueba

--- carpeta js/buscador.js
+++ carpeta js/buscador.js
@@ -3,7 +3,8 @@
 
 // });
 
-let apiKey = 'https://api.themoviedb.org/3/movie/76341?api_key=1c7b96c9c6844bd81ab3f6d24f285c12'
+let apiKey = 'https://api.themoviedb.org/3/movie/76341?api_key=1c7b96c9c6844bd81ab3f6d24f285c12';
+let buscador = document.querySelector('.buscador');
 
 fetch(apiKey)
 	.then(function(response){
@@ -11,6 +12,7 @@ fetch(apiKey)
 	})
 	.then(function(data){
 			console.log(data);
+			let busqueda 
 	})
 	.catch(function(error){
 			console.log('El error es: ' + error);
