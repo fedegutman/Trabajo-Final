78460bc523cc3a4f6bf615d275324a95dd6b3c0e
Moavro <tmoavro@udesa.edu.ar>
2022-11-22 17:17:10 -0300

    detalle generos

--- carpeta js/detalle_generos.js
+++ carpeta js/detalle_generos.js
@@ -1,29 +1,28 @@
 let qs = location.search
 let qs_OL= new URLSearchParams(qs);
 
-let idPelis = qs_OL.get('id_peliculas')
-let idSeries = qs_OL.get('id_series')
+let id = qs_OL.get('id')
 
 
-let url_Pelis = `https://api.themoviedb.org/3/discover/movie?api_key=35664717fe783f635e22f58af930e36f&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=${idPelis}&with_watch_monetization_types=flatrate`
 
-let url_series = `https://api.themoviedb.org/3/discover/tv?api_key=35664717fe783f635e22f58af930e36f&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=${idSeries}&with_watch_monetization_types=flatrate`
+let url_Pelis_generos = `https://api.themoviedb.org/3/discover/movie?api_key=35664717fe783f635e22f58af930e36f&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=${id}&with_watch_monetization_types=flatrate`
+
+let url_series_generos = `https://api.themoviedb.org/3/discover/tv?api_key=35664717fe783f635e22f58af930e36f&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=${id}&with_watch_monetization_types=flatrate`
 
 
-fetch(url_Pelis)
+fetch(url_Pelis_generos)
 .then(function(response){
     return response.json();
 })
 .then(function(data){
-    console.log(data);
     let info = data.results
-    console.log(info);
-    let container = document.querySelector('.seccion_generos');
+    let container = document.querySelector('.peliculas_generos');
     let peliculas = '';
-
+    let titulo = document.querySelector('.titulo_genero')
+    titulo.innerHTML = info[id].name
+    console.log(info[id].title)
     for(let i=0; i < info.length; i++){
         let longitudTitulo = info[i].title.length
-        console.log(longitudTitulo)
         if (longitudTitulo >= 25){
             peliculas += `<article class='peliculasjs'>
                             <a href= "detalle_peliculas.html?id=${info[i].id}">
@@ -42,6 +41,43 @@ fetch(url_Pelis)
     }
     
     container.innerHTML = peliculas;  
+})
+.catch(function(error){
+    console.log('El error es: ' + error);
+})
+
+
+
+fetch(url_series_generos)
+.then(function(response){
+    return response.json();
+})
+.then(function(data){
+    let info = data.results
+    console.log(info)
+    let container = document.querySelector('.series_generos');
+    let series = '';
+
+    for(let i=0; i < info.length; i++){
+        let longitudTitulo = info[i].name.length
+        if (longitudTitulo >= 25){
+            series += `<article class='peliculasjs'>
+                            <a href= "detalle_peliculas.html?id=${info[i].id}">
+                                <h3 class='titulo_largo'>${info[i].name}</h3>
+                                <img class = 'pelis' src=https://image.tmdb.org/t/p/w500/${info[i].poster_path} alt='' />
+                            </a>
+                            </article>`
+        } else {
+            series += `<article class='peliculasjs'>
+                            <a href= "detalle_peliculas.html?id=${info[i].id}">
+                                <h3>${info[i].name}</h3>
+                                <img class = 'pelis' src=https://image.tmdb.org/t/p/w500/${info[i].poster_path} alt='' />
+                            </a>
+                            </article>`
+        };
+    }
+    
+    container.innerHTML = series;  
 })
 .catch(function(error){
     console.log('El error es: ' + error);
